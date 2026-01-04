9bbfe6660341d335af4d59e76f4a1d4e3bace6c7
Ian Njuguna <ian.njuguna@student.moringaschool.com>
2026-01-04 21:53:49 +0300

    Fix: Remove nested Router (White Screen) and update branding

--- front_end/src/main.jsx
+++ front_end/src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// 1. Import BrowserRouter
-import { BrowserRouter } from 'react-router-dom'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* 2. Wrap your App inside it */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
